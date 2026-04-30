12049fb7774921ede04174577a8d3ae315fbf57c
Khibullin <youremail@example.com>
2026-04-30 11:30:42 +0500

    Fix cross-origin session settings

--- backend/settings.py
+++ backend/settings.py
@@ -82,16 +82,20 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     'https://zpt.kz',
     'https://www.zpt.kz',
     'https://zpt-kz.onrender.com',
 ]
+
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
 
 CSRF_TRUSTED_ORIGINS = [
     'https://zpt.kz',
