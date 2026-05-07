1ab185827ab571ea9804f7604a3bf021f122a91d
Khibullin <youremail@example.com>
2026-05-07 11:08:20 +0500

    Add service request frontend routes

--- backend/urls.py
+++ backend/urls.py
@@ -5,8 +5,29 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    # API
     path('api/', include('core.urls')),  # запчасти
     path('api/service/', include('service_requests.urls')),  # СТО / детейлинг
 
-    path('service-request/', TemplateView.as_view(template_name='service-request/index.html')),
+    # frontend service-request
+    path(
+        'service-request/',
+        TemplateView.as_view(
+            template_name='service-request/index.html'
+        )
+    ),
+
+    path(
+        'service-request/cabinet/',
+        TemplateView.as_view(
+            template_name='service-request/cabinet/index.html'
+        )
+    ),
+
+    path(
+        'service-request/register/',
+        TemplateView.as_view(
+            template_name='service-request/register/index.html'
+        )
+    ),
 ]
