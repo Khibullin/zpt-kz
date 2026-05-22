1f7dfe9c9ae919d69a1ad77ee15e775f36dc9707
Khibullin <youremail@example.com>
2026-05-22 09:48:17 +0500

    Add public zpt parts sellers routes

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+
+from core.views import (
+    parts_sellers_catalog,
+    parts_seller_detail,
+)
+
 from service_requests.views import (
     service_request_result,
     services_catalog,
     service_seller_detail,
 )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +21,35 @@ urlpatterns = [
     path('api/', include('core.urls')),  # запчасти
     path('api/service/', include('service_requests.urls')),  # СТО / детейлинг
 
-    # frontend service-request
+    # FRONTEND REQUEST PARTS
+    path(
+        'request-parts/',
+        TemplateView.as_view(
+            template_name='request-parts/index.html'
+        )
+    ),
+
+    path(
+        'request-parts/cabinet/',
+        TemplateView.as_view(
+            template_name='request-parts/cabinet/index.html'
+        )
+    ),
+
+    # КАТАЛОГ ПРОДАВЦОВ ЗАПЧАСТЕЙ (НОВЫЙ КРАСИВЫЙ URL)
+    path(
+        'parts-sellers/',
+        parts_sellers_catalog,
+        name='parts_sellers_catalog_public',
+    ),
+
+    path(
+        'parts-seller/<int:seller_id>/',
+        parts_seller_detail,
+        name='parts_seller_detail_public',
+    ),
+
+    # СТО / ДЕТЕЙЛИНГ
     path(
         'service-request/',
         TemplateView.as_view(
