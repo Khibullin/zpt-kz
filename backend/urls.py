aa8dba5007a4171ae26a2aa077aa1bf174c6816f
Khibullin <youremail@example.com>
2026-06-16 17:37:00 +0500

    Fix product images

--- backend/urls.py
+++ backend/urls.py
@@ -95,3 +95,18 @@ urlpatterns = [
 ]
 
 # parts sellers routes
+
+from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
+
+
+urlpatterns += [
+    re_path(
+        r'^products/(?P<path>.*)$',
+        serve,
+        {
+            'document_root': settings.BASE_DIR / 'products',
+        }
+    ),
+]
